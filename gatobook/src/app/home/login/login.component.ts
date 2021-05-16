39d21b4ac764a5b269ca7638c33917e1fde92b16
gabriel <gabriellopes112009@gmail.com>
2021-05-16 12:10:32 -0300

    implementaçao do template form - angular

--- gatobook/src/app/home/login/login.component.ts
+++ gatobook/src/app/home/login/login.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  usuario = ''
+  senha = ''
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  login(){
+    console.log(`
+      usuario = ${this.usuario},
+      senha = ${this.senha}
+    `)
+  }
 }
