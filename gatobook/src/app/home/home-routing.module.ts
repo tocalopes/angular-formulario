c5345b57d6233b6372c735ea8b3c3a5ac99a3ad5
gabriel <gabriellopes112009@gmail.com>
2021-05-16 11:41:59 -0300

    Criação do componente home, e de logind, lazy loading

--- gatobook/src/app/home/home-routing.module.ts
+++ gatobook/src/app/home/home-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component: HomeComponent,
+    children: [
+      {
+        path:'',
+        component: LoginComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
