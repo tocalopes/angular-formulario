e6024af84c1408a6ed2aebc6636ef49fd5badd32
gabriel <gabriellopes112009@gmail.com>
2021-06-06 13:25:18 -0300

    Validator customizado

--- gatobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -2,6 +2,7 @@ import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
+import { minusculoValidator } from './minusculo.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -28,7 +29,9 @@ export class NovoUsuarioComponent implements OnInit {
         Validators.required,
         Validators.minLength(4)
       ],
-        userName:[''],
+        userName:['',
+        minusculoValidator
+      ],
         password:['',
         Validators.minLength(3)
       ]
