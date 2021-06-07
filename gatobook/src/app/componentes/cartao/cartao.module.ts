6b30c06c2ef0eb00d8891a8273ff7de9a49da010
gabriel <gabriellopes112009@gmail.com>
2021-06-07 13:14:55 -0300

    Criação do componente Animal para foto e inicio da cosntrução do card

---
+++ gatobook/src/app/componentes/cartao/cartao.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartaoComponent } from './cartao.component';
+
+
+
+@NgModule({
+  declarations: [
+    CartaoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CartaoModule { }
