41553d13a08062e33e374f75d25bef499f53fdf1
gabriel <gabriellopes112009@gmail.com>
2021-06-13 13:59:31 -0300

    Mundança para desenvolvimento do componentes de comentário, desenvolvimento do seu template

--- gatobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
+++ gatobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comentarios } from './comentario';
 
 @Component({
   selector: 'app-comentarios',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comentarios.component.scss']
 })
 export class ComentariosComponent implements OnInit {
+
+  comentarios$! :Observable<Comentarios>;
+
 
   constructor() { }
 
